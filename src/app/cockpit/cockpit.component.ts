6cec5c67da24c8c9a641c285ead2d1295b880111
Matthew Waskin <matthew@waskin.com>
2023-07-12 15:26:52 -0400

    view encapsulation, local references, ViewChild and ContentChild, ng-content, and lifestyle hooks

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -16,20 +22,26 @@ export class CockpitComponent {
     serverName: string;
     serverContent: string;
   }>();
-  newServerName = '';
-  newServerContent = '';
 
-  onAddServer() {
+  // newServerName = ''; using local reference to access serverName now instead of 2-way binding
+  // newServerContent = '';
+
+  /* @ViewChild decorator creates an element reference from the template element matching the selector (or local reference) string passed to the decorator; can also be passed a component type (such as CockpitComponent) used in the template of this component */
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
+
+  /* give the methods a nameInput parameter that the local reference in the template can be passed to as an argument when these methods are called. Then, it's properties (value) can be read and assigned to the serverName in the event data output by the serverCreated event */
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,87 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // None, ShadowDom, Emulated (default)
 })
-export class ServerElementComponent {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   /* @Input('optionalAliasName') decorator makes a property accessible within any parent components (using it's alias, if assigned), instead of only within this component's template;
   Allows the property value to be input by the parent component */
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text Content:' + this.header.nativeElement.textContent);
+    console.log(
+      'Text Content of paragraph: ' + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+    console.log(
+      'Text Content of paragraph: ' + this.paragraph.nativeElement.textContent
+    );
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
 }
